76a4392d1566f12a1c140e4a39eb6dde25e99223
Herelius <youcefboutrig92@gmail.com>
2024-11-21 10:03:44 +0100

    Add a view

--- search/views.py
+++ search/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+
+def index(request):
+    return HttpResponse("<h1>Hey ! tu es sur l'index de l'application search du projet NutriFood.</h1>")
 
 # Create your views here.
