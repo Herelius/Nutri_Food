3250e2cac9f3b00c13935c4368fc607cb6a8434f
Herelius <youcefboutrig92@gmail.com>
2024-11-25 13:00:38 +0100

    Add a manager command 'api_openfood' to get food data and save it in the database

--- search/management/commands/api_openfood.py
+++ search/management/commands/api_openfood.py
@@ -1,12 +1,30 @@
-from django.core.management.base import BaseCommand
-import requests
 import json
+import requests
+
+from django.core.management.base import BaseCommand
+
+from search.models import Product
 
 
 class Command(BaseCommand):
-    help = "Custom command"
+    help = "Get Products"
 
     def handle(self, *args, **options):
-        url = 'https://world.openfoodfacts.net/api/v2/product/3017620422003'
-        request = requests.get(url)
-        self.stdout.write(self.style.SUCCESS(json.dumps(request.json(), indent=4)))
+        list_categories = ['chocolates', 'cheeses', 'sodas']
+
+        for category in list_categories:
+            url = f'https://world.openfoodfacts.net/api/v2/search?categories_tags_en={category}&fields=product_name,nutriscore_data,categories_tags,nutriscore_data'
+            request = requests.get(url)
+            liste_API_products = request.json()['products']
+
+            for api_product in liste_API_products:
+                if "nutriscore_data" in api_product:
+                    product = Product(
+                        name=api_product["product_name"],
+                        nutri_score=api_product["nutriscore_data"]["grade"],
+                        )
+                    product.save()
+
+                self.stdout.write(self.style.SUCCESS(f'  Enregistrement du produit {api_product["product_name"]} : OK'))
+            
+        self.stdout.write(self.style.SUCCESS('Enregistrement des produits dans la Base de données : OK'))
--- search/models.py
+++ search/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 class Product(models.Model):
-    name = models.CharField(max_length=150)
-    ingredient = models.CharField(max_length=250)
-    nutri_score = models.CharField(max_length=1)
-    image = models.URLField()
+    name = models.CharField(max_length=150, unique=True)
+    # ingredient = models.CharField(max_length=250)
+    nutri_score = models.CharField(max_length=2)
+    # image = models.URLField()
